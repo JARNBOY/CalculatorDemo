5e938d1694cd7776b36a93dd897791dcf69e735b
Papon <papon.s@lawyerplusgroup.com>
2023-01-27 13:25:27 +0700

    [How to create Framework for iOS in 2023 (basic)][First Option: Drag framework project to Main Application project] Create CalculatorDemo project for sample explain import framework (Reviewed by Papon S.)

--- CalculatorDemo/ViewController.swift
+++ CalculatorDemo/ViewController.swift
@@ -6,12 +6,21 @@
 //
 
 import UIKit
+import LogicCalculate
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var lblCalculator:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let plusCalNumber = Calculate.plusNumber(n1: 1, n2: 1)
+        let minusCalNumber = Calculate.plusNumber(n1: 1, n2: 1)
+        
+        let calPlusFromFrameworkText = "Plus number 1+1 use framework is = \(plusCalNumber)"
+        let calMinusFromFrameworkText = "Plus number 1-1 use framework is = \(minusCalNumber)"
+        lblCalculator.text = "\(calPlusFromFrameworkText)\n\(calMinusFromFrameworkText)"
     }
 
 
